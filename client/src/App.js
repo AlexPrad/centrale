58d5c462e35a5b90823a0e9970a7639a6f1ca4ad
leajacob <66574215+leajacob@users.noreply.github.com>
2020-06-11 10:37:14 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import recommande from "./components/recommande";
-import PokemonDisplayer from "./components/movielist";
-import SubmitEventStory from "./components/ajout";
+import recko from "./components/recommande";
+import PokemonDisplayer from "./components/movielist2";
+import SubmitEventStory from "./components/MoviePage2";
+import "./App.css"
 
 function App() {
+  const [selectedUser, setSelectedUser] = useState('emma')
   return (
     <Router>
-      <div> 
-        <div><h1> Nom du site </h1></div>
+      <div className="background"> 
+        <div className = "title"><h2> Adopte un film </h2></div>
         <nav>
           <ul>
             <li>
-              <Link to="/">Accueil</Link>
+              <Link className = "links" to="/">Accueil</Link>
             </li>
             <li>
-              <Link to="/liste">Liste de films</Link>
+              <Link className = "links" to="/liste">Liste de films</Link>
             </li>
             <li>
-              <Link to="/reco"> Recommande-moi un film </Link>
+              <Link className = "links" to="/reco"> Recommande-moi un film </Link>
             </li>
             <li>
-              <Link to="/ajout"> Ajouter un film </Link>
+              <Link className = "links" to="/ajout"> Ajouter un film </Link>
             </li>
           </ul>
         </nav>
@@ -30,7 +32,7 @@ function App() {
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/liste">
-            <PokemonDisplayer />
+            <PokemonDisplayer user={selectedUser}/>
           </Route>
           <Route path="/reco">
             <recommande />
